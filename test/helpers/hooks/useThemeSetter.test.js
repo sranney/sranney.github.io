01b5089e271671bc6beb614a09ff775c0a7b09e1
spencer ranney <spencer.ranney@coxautoinc.com>
2019-11-11 14:57:27 -0600

    test for useThemeSetter

--- test/helpers/hooks/useThemeSetter.test.js
+++ test/helpers/hooks/useThemeSetter.test.js
@@ -1,5 +1,16 @@
-describe("useFetchData", () => {
-	it("should do something", () => {
-		
-	})
+import useThemeSetter from "../../../src/helpers/hooks/useThemeSetter";
+
+describe("useThemeSetter", () => {
+	it("should display theme with initial value from local storage", () => {
+
+	});
+	it("should display toggled theme with initial value from local storage", () => {
+
+	});
+	it("should display theme with initial value from primary parameter", () => {
+
+	});
+	it("should display toggled theme with initial value from primary parameter", () => {
+
+	});
 });
